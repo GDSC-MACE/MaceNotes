2130035f0327c14d21e8cc1708148e63dea5b2df
niv3D <nived807@gmail.com>
2021-12-11 03:03:39 +0530

    niv3d-1
    some responsive reworks in
     >Full layout
     >Landing page
    made page to display notes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import './Pages/Contribute/Init/Init.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Pages/Home/home";
 import Contribute1 from "./Pages/Contribute/Init/Init";
 import Contribute2 from "./Pages/Contribute/Final/Final";
 import First from "./Pages/First/first";
 import EC from "./Pages/EC/ec";
-import CS from "./Pages/CS/cs";
 import MECH from "./Pages/MECH/mech";
 import EEE from "./Pages/EEE/eee";
 import CIVIL from "./Pages/CIVIL/civil";
 import Nomatch from "./Pages/Nomatch/Nomatch";
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import CS from "./Pages/CS/CS";
 
 function App() {
   return (
-    <div>
-    <Navbar />
-    <div style={{'marginLeft': "8%"}}>
-    <Router>
-        <Switch>
-          <Route exact path={["/", "/index", "/home"]} component={Home} />
-          <Route exact path="/first" component={First} />
-          <Route exact path="/cs" component={CS} />
-          <Route exact path="/ec" component={EC} />
-          <Route exact path="/eee" component={EEE} />
-          <Route exact path="/civil" component={CIVIL}/>
-          <Route exact path="/mech" component={MECH} />
-          <Route exact path="/contribute1" component={Contribute1} />
+    <div className="App">
+      <Navbar />
+      <div className="Main-container">
+        <Router>
+          <Switch>
+            <Route exact path={["/", "/index", "/home"]} component={Home} />
+            <Route exact path="/cs" component={CS} />
+            <Route exact path="/first" component={First} />
+            <Route exact path="/ec" component={EC} />
+            <Route exact path="/eee" component={EEE} />
+            <Route exact path="/civil" component={CIVIL} />
+            <Route exact path="/mech" component={MECH} />
+            <Route exact path="/contribute1" component={Contribute1} />
             <Route exact path="/contribute2" component={Contribute2} />
             <Route path="*" component={Nomatch} />
-        </Switch>
-      </Router>
+          </Switch>
+        </Router>
+      </div>
+      <Footer />
     </div>
-
-    </div>
-
   );
 }
 
--- src/Pages/CS/cs.js
+++ src/Pages/CS/cs.js
@@ -1,11 +1,76 @@
+import "./CS.css";
 
-
-function Cs(){
-    return (
-        <div>
-            CS
+function CS() {
+  return (
+    <>
+      <div className="CS">
+        <div className="main-title">
+          <div className="title">
+            <h1 id="dep-title">COMPUTER SCIENCE</h1>
+          </div>
+          <div className="sem">
+            <button className="sem-button">Semester 1</button>
+            <button className="sem-button">Semester 2</button>
+            <button className="sem-button">Semester 3</button>
+            <button className="sem-button">Semester 4</button>
+            <button className="sem-button">Semester 5</button>
+            <button className="sem-button">Semester 6</button>
+            <button className="sem-button">Semester 7</button>
+            <button className="sem-button">Semester 8</button>
+          </div>
         </div>
-
-    )
+        <div className="contents">
+          <div className="notes">
+            <h2 className="subject-name">COA</h2>
+            <a href="">MODULE 1</a>
+            <a href="">MODULE 2</a>
+            <a href="">MODULE 3</a>
+            <a href="">MODULE 4</a>
+            <a href="">MODULE 5</a>
+          </div>
+          <div className="notes">
+            <h2 className="subject-name">GRAPH THEORY</h2>
+            <a href="">MODULE 1</a>
+            <a href="">MODULE 2</a>
+            <a href="">MODULE 3</a>
+            <a href="">MODULE 4</a>
+            <a href="">MODULE 5</a>
+          </div>
+          <div className="notes">
+            <h2 className="subject-name">OS</h2>
+            <a href="">MODULE 1</a>
+            <a href="">MODULE 2</a>
+            <a href="">MODULE 3</a>
+            <a href="">MODULE 4</a>
+            <a href="">MODULE 5</a>
+          </div>
+          <div className="notes">
+            <h2 className="subject-name">DBMS</h2>
+            <a href="">MODULE 1</a>
+            <a href="">MODULE 2</a>
+            <a href="">MODULE 3</a>
+            <a href="">MODULE 4</a>
+            <a href="">MODULE 5</a>
+          </div>
+          <div className="notes">
+            <h2 className="subject-name">HONOURS</h2>
+            <a href="">MODULE 1</a>
+            <a href="">MODULE 2</a>
+            <a href="">MODULE 3</a>
+            <a href="">MODULE 4</a>
+            <a href="">MODULE 5</a>
+          </div>
+          <div className="notes">
+            <h2 className="subject-name">MINORS</h2>
+            <a href="">MODULE 1</a>
+            <a href="">MODULE 2</a>
+            <a href="">MODULE 3</a>
+            <a href="">MODULE 4</a>
+            <a href="">MODULE 5</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Cs;
+export default CS;
